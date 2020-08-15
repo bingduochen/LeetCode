54977cb9d38f7b10bc9ec0b34b585e6b1e0bd838
bingduochen <bingduochen@tencent.com>
2020-08-16 00:55:34 +0800

    add

---
+++ src/main/offer/Solution3.java
@@ -0,0 +1,20 @@
+package main.offer;
+
+public class Solution3 {
+    public int findRepeatNumber(int[] nums) {
+        int tmp = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] != i) {
+                if(nums[i]==nums[nums[i]]){
+                    return nums[i];
+                }
+
+                tmp = nums[i];
+                nums[i] = nums[nums[i]];
+                nums[tmp] = tmp;
+            }
+        }
+        return 0;
+
+    }
+}
